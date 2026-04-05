cce3318c9686228646afc40218f8a2e1cec70537
Dhruvi Mokani <dhruvimokani08@gmail.com>
2026-04-05 19:27:30 +0530

    p34 final

--- p34.cpp
+++ p34.cpp
@@ -1,115 +1,187 @@
 #include<iostream>
 using namespace std;
 
-//FIND MAX 
 template<typename T>
-T findMAX(T arr[] ,int n)
+T findMAX(T arr[], int n)
 {
-    T Max=arr[0];
-    for(int i=0;i<n;i++)
+    T Max = arr[0];
+    for(int i = 1; i < n; i++)
     {
-        if(arr[i]>Max)
+        if(arr[i] > Max)
         {
-            Max=arr[i];
+            Max = arr[i];
         }
     }
     return Max;
 }
 
-//REVERSE ARRAY
+template<typename T>
+T findMIN(T arr[], int n)
+{
+    T Min = arr[0];
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] < Min)
+        {
+            Min = arr[i];
+        }
+    }
+    return Min;
+}
+
 template<typename T>
 void reverseArray(T arr[], int n)
 {
-    for(int i=0;i<n/2;i++)
+    for(int i = 0; i < n/2; i++)
     {
-T temp=arr[i];
-arr[i]=arr[n-i-1];
-arr[n-i-1]=temp;
+        T temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
     }
 }
 
-//DISPLAY
 template<typename T>
 void Display(T arr[], int n)
 {
-    for(int i=0;i<n;i++)
+    for(int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
-        cout << endl;
     }
+    cout << endl;
+}
+
+template<typename T>
+void findLeaders(T arr[], int n)
+{
+    cout << "Leader Elements: ";
+    T leader = arr[n-1];
+    cout << leader << " ";
+
+    for(int i = n-2; i >= 0; i--)
+    {
+        if(arr[i] > leader)
+        {
+            leader = arr[i];
+            cout << leader << " ";
+        }
+    }
+    cout << endl;
+}
+
+template<typename T>
+int searchElement(T arr[], int n, T key)
+{
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+            return i;
+    }
+    return 0;
+}
+
+template<typename T>
+int countOccurrences(T arr[], int n, T key)
+{
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == key)
+            count++;
+    }
+    return count;
 }
 
 int main()
 {
-int ch;
+    int ch;
 
-cout<<"1 for int arrray"<<endl<<"2 for float array"<<endl<<"3 for char array"<<endl;
-cin>>ch;
+    cout<<"1 for int array\n2 for float array\n3 for char array\n";
+    cin>>ch;
 
-switch(ch)
-{
-case 1:
-{int s;
- cout<<"Enter How many Elements You want to add in array:"<<endl;
-cin>>s;
-int a[s];
-cout<<"Enter Array Elements:"<<endl;
-for(int i=0;i<s;i++)
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+
+    switch(ch)
     {
-       cin >> a[i];
-    }
-int Maximum=findMAX(a,s);
-cout<<"\nMaximum Element:"<<Maximum<<endl;
-
-reverseArray(a,s);
-
-cout<<endl;
-Display(a,s);
-break;
-}
-case 2: 
-{int m;
-cout<<"Enter How many Elements You want to add in array:"<<endl;
-cin>>m;
-float b[m];
-cout<<"Enter Array Elements:"<<endl;
-for(int i=0;i<m;i++)
+    case 1:
     {
-       cin >> b[i];
+        int arr[n];
+        cout<<"Enter elements:\n";
+        for(int i=0;i<n;i++) cin>>arr[i];
+
+        cout<<"Maximum: "<<findMAX(arr,n)<<endl;
+        cout<<"Minimum: "<<findMIN(arr,n)<<endl;
+
+        reverseArray(arr,n);
+        cout<<"Reversed Array: ";
+        Display(arr,n);
+
+        findLeaders(arr,n);
+
+        int key;
+        cout<<"Enter element to search: ";
+        cin>>key;
+
+        int pos = searchElement(arr,n,key);
+        cout<<"Position: "<<pos<<endl;
+
+        cout<<"Occurrences: "<<countOccurrences(arr,n,key)<<endl;
+        break;
     }
-float maximum=findMAX(b,m);
-cout<<"\nMaximum Element:"<<maximum<<endl;
 
-reverseArray(b,m);
-
-cout<<endl;
-Display(b,m);
-break;
-}
-case 3: 
-{int y;
-cout<<"Enter How many Elements You want to add in array:"<<endl;
-cin>>y;
-char x[y];
-cout<<"Enter Array Elements:"<<endl;
-for(int i=0;i<y;i++)
+    case 2:
     {
-       cin >> x[i];
+        float arr[n];
+        cout<<"Enter elements:\n";
+        for(int i=0;i<n;i++) cin>>arr[i];
+
+        cout<<"Maximum: "<<findMAX(arr,n)<<endl;
+        cout<<"Minimum: "<<findMIN(arr,n)<<endl;
+
+        reverseArray(arr,n);
+        cout<<"Reversed Array: ";
+        Display(arr,n);
+
+        findLeaders(arr,n);
+
+        float key;
+        cout<<"Enter element to search: ";
+        cin>>key;
+
+        int pos = searchElement(arr,n,key);
+        cout<<"Position: "<<pos<<endl;
+
+        cout<<"Occurrences: "<<countOccurrences(arr,n,key)<<endl;
+        break;
     }
-char max=findMAX(x,y);
-cout<<"\nMaximum Element:"<<max<<endl;
 
-reverseArray(x,y);
+    case 3:
+    {
+        char arr[n];
+        cout<<"Enter elements:\n";
+        for(int i=0;i<n;i++) cin>>arr[i];
 
-cout<<endl;
-Display(x,y);
+        cout<<"Maximum: "<<findMAX(arr,n)<<endl;
+        cout<<"Minimum: "<<findMIN(arr,n)<<endl;
 
-break;
-}
-}
+        reverseArray(arr,n);
+        cout<<"Reversed Array: ";
+        Display(arr,n);
 
+        findLeaders(arr,n);
 
+        char key;
+        cout<<"Enter element to search: ";
+        cin>>key;
 
+        int pos = searchElement(arr,n,key);
+        cout<<"Position: "<<pos<<endl;
+
+        cout<<"Occurrences: "<<countOccurrences(arr,n,key)<<endl;
+        break;
+    }
+    }
 
     return 0;
 }
