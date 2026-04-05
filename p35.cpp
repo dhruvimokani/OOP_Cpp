019df673cd3966e6c8471bdea62fee070ed17ebb
Dhruvi Mokani <dhruvimokani08@gmail.com>
2026-04-05 19:12:57 +0530

    p35 final

--- p35.cpp
+++ p35.cpp
@@ -1,44 +1,69 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 using namespace std;
+
+int depth = 0;
+int maxDepth;
+
 int superdigit(int n)
 {
-    if(n<10)
-    {return n;}
-    int Sum=0;
-    while(n>0)
-    {
-        int c=n%10;
-        n=n/10;
-        Sum+=c;
-    }
-    superdigit(Sum);
-return Sum;
-}
+    depth++;
 
+    cout << "Level " << depth << " -> Current number: " << n << endl;
+
+    if(depth > maxDepth)
+    {
+        cout << "Stopped early at level limit!" << endl;
+        return n;
+    }
+
+    if(n < 10)
+    {
+        return n;
+    }
+
+    int Sum = 0;
+
+    while(n > 0)
+    {
+        int c = n % 10;
+        n = n / 10;
+        Sum += c;
+    }
+    cout << "Sum at Level " << depth << " = " << Sum << endl;
+
+    return superdigit(Sum);
+}
 
 int main()
 {
- int sum=0;
-string str;
+    string str;
+    long long sum = 0;
 
-cout<<"Enter String:";
-cin>>str;
+    cout << "Enter String: ";
+    cin >> str;
 
-int len=str.length();
-cout<<"String Length:"<<len<<endl;
+    int len = str.length();
+    cout << "String Length: " << len << endl;
 
+    for(int i = 0; i < len; i++)
+    {
+        sum = sum + (str[i] - '0');
+    }
 
-for(int i=0;i<len;i++)
-{
-sum=sum+(str[i]-'0');
-}
-int k;
-cout<<endl;
-cout<<"How many times it is concatenated:";
-cin>>k;
-sum=sum*k;
-sum=superdigit(sum);
-cout<<sum;
-return 0;
+    int k;
+    cout << "\nHow many times it is concatenated: ";
+    cin >> k;
+
+    sum = sum * k;
+
+    cout << "Enter max recursion depth: ";
+    cin >> maxDepth;
+
+    int result = superdigit(sum);
+
+    cout << "\nFinal Super Digit: " << result << endl;
+    cout << "Total Recursion Depth Used: " << depth << endl;
+
+    return 0;
 }
